27c44fa644affde57b4c698a84e85f5d48d4946d
Megan McKissack <meganmckissack@gmail.com>
2022-05-19 21:38:08 -0700

    create GalacticAge class with passing test

---
+++ src/galacticAge.js
@@ -0,0 +1,10 @@
+export class GalacticAge {
+  constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge, lifeExpectancy) {
+    this.earthAge = earthAge;
+    this.mercuryAge = mercuryAge;
+    this.venusAge = venusAge;
+    this.marsAge = marsAge;
+    this.jupiterAge = jupiterAge;
+    this.lifeExpectancy = lifeExpectancy;
+  }
+}
