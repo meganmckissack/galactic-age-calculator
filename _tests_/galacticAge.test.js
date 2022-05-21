a59329fba920a1bdd4e941814011dd471e1459e5
Megan McKissack <meganmckissack@gmail.com>
2022-05-20 18:23:56 -0700

    add beforeEach methed to create a resuable myGalacticAge object and removed local object in first test

--- _tests_/galacticAge.test.js
+++ _tests_/galacticAge.test.js
@@ -1,9 +1,12 @@
 import { GalacticAge } from "../src/galacticAge.js";
 
 describe('GalacticAge', () => {
+  let myGalacticAge;
+  beforeEach(() => {
+    myGalacticAge = new GalacticAge(42,175,68,22,4,100);
+  });
 
   test('it should create a galactic age class that has 6 age properties', () => {
-    let myGalacticAge = new GalacticAge(42,175,68,22,4,100);
 
     expect(myGalacticAge.earthAge).toEqual(42);
     expect(myGalacticAge.mercuryAge).toEqual(175);
@@ -12,4 +15,6 @@ describe('GalacticAge', () => {
     expect(myGalacticAge.jupiterAge).toEqual(4);
     expect(myGalacticAge.lifeExpectancy).toEqual(100);
   });
+
+
 });
