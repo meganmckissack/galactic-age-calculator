d09970aed94450e4b3c68ca4613be40632a50bdd
Megan McKissack <meganmckissack@gmail.com>
2022-05-20 19:39:33 -0700

    update galacticAge class with new property for health

--- _tests_/galacticAge.test.js
+++ _tests_/galacticAge.test.js
@@ -3,16 +3,17 @@ import { GalacticAge } from "../src/galacticAge.js";
 describe('GalacticAge', () => {
   let myGalacticAge;
   beforeEach(() => {
-    myGalacticAge = new GalacticAge(42,175,68,22,4,100);
+    myGalacticAge = new GalacticAge(42,175,68,22,4,50,100);
   });
 
-  test('it should create a galactic age class that has 6 age properties', () => {
+  test('it should create a galactic age class that has 7 age properties', () => {
 
     expect(myGalacticAge.earthAge).toEqual(42);
     expect(myGalacticAge.mercuryAge).toEqual(175);
     expect(myGalacticAge.venusAge).toEqual(68);
     expect(myGalacticAge.marsAge).toEqual(22);
     expect(myGalacticAge.jupiterAge).toEqual(4);
+    expect(myGalacticAge.healthScore).toEqual(50);
     expect(myGalacticAge.lifeExpectancy).toEqual(100);
   });
 
@@ -36,4 +37,5 @@ describe('GalacticAge', () => {
     expect(myGalacticAge.jupiterAge).toEqual(4);
   });
 
+  
 });
